122022fd2e79cdf59d2a1d2251030cdfd084aece
Fosco Marotto <gfosco@gmail.com>
2013-05-26 14:26:02 -0700

    Sync update 1

--- templateProjectSync.js
+++ templateProjectSync.js
@@ -10,8 +10,7 @@ var http = require('http'),
 	fs = require('fs'),
 	execSync = require('execSync');
 
-var Templates = {'QWERTYPROD':101};
-var Replace = {
+var Templates = {
 	'QWERTYPROD' : {
 		'product' : 'QWERTYPROD',
 		'organization' : 'QWERTYORG',
@@ -53,15 +52,14 @@ function makeTemplate(options) {
 
 	if (Templates[template]) {
 
-		var old_product = Replace[template]['product'];
-		var old_org = Replace[template]['organization'];
-		var old_bundle = Replace[template]['bundle'];
-		var old_appid = Replace[template]['parseAppId'];
-		var old_key = Replace[template]['parseKey'];
-		var old_name = Replace[template]['name'];
+		var old_product = Templates[template]['product'],
+			old_org = Templates[template]['organization'],
+			old_bundle = Templates[template]['bundle'],
+			old_appid = Templates[template]['parseAppId'],
+			old_key = Templates[template]['parseKey'],
+			old_name = Templates[template]['name'];
 
 		var ourToken = rack();
-		console.log('Unique token ' + ourToken + ' generated.');
 
 		var template_folder = './xcode_projects/' + template + '/'
 		var working_folder = './working_folder/' + ourToken + '/';
@@ -69,40 +67,39 @@ function makeTemplate(options) {
 
 		// Let's start by just setting up directories:
 		var result = execSync.exec('mkdir ' + working_folder);
-		if (result.code) failExit('Return code ' + result.code + ' while making working_folder for token: ' + ourToken);
+		if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while making working_folder for token: ' + ourToken);
 
 		result = execSync.exec('mkdir ' + output_folder);
-		if (result.code) failExit('Return code ' + result.code + ' while making output_folder for token: ' + ourToken);
+		if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while making output_folder for token: ' + ourToken);
 
 		var copyCommand = 'cd ' + template_folder + ' && cp -R * ../.' + working_folder + ' && cd ../..';	
-		console.log(copyCommand);	
 		result = execSync.exec(copyCommand);
-		if (result.code) failExit('Return code ' + result.code + ' while copying template for token: ' + ourToken);
+		if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while copying template for token: ' + ourToken);
 
 		result = execSync.exec('mv ' + working_folder + old_product + ' ' + working_folder + product);
-		if (result.code) failExit('Return code ' + result.code + ' while renaming product folder.');
+		if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while renaming product folder.');
 
 		result = execSync.exec('mv ' + working_folder + old_product + '.xcodeproj ' + working_folder + product + '.xcodeproj');
-		if (result.code) failExit('Return code ' + result.code + ' while renaming product.xcodeproj folder.');
+		if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while renaming product.xcodeproj folder.');
 
 		result = execSync.exec('mv ' + working_folder + product + '/' + old_product + '-Prefix.pch ' + working_folder + product + '/' + product + '-Prefix.pch');
-		if (result.code) failExit('Return code ' + result.code + ' while renaming product-Prefix.pch file.');
+		if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while renaming product-Prefix.pch file.');
 
 		result = execSync.exec('mv ' + working_folder + product + '/' + old_product + '-Info.plist ' + working_folder + product + '/' + product + '-Info.plist');
-		if (result.code) failExit('Return code ' + result.code + ' while renaming product-Info.plist file.');
+		if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while renaming product-Info.plist file.');
 
 		var xcpj = fs.readFileSync(working_folder + product + '.xcodeproj/project.pbxproj','utf8');
 		xcpj = xcpj.replace(new RegExp(old_product,'g'),product);
 		xcpj = xcpj.replace(new RegExp(old_org,'g'),org);
 		result = fs.writeFileSync(working_folder + product + '.xcodeproj/project.pbxproj',xcpj,'utf8');
-		if (result) failExit('Result returned while overwriting product.xcodeproj/project.pbxproj.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product.xcodeproj/project.pbxproj.');
 
 		var appdh = fs.readFileSync(working_folder + product + '/AppDelegate.h','utf8');
 		appdh = appdh.replace(new RegExp(old_product,'g'),product);
 		appdh = appdh.replace(new RegExp(old_org,'g'),org);
 		appdh = appdh.replace(new RegExp(old_name,'g'),name);
 		result = fs.writeFileSync(working_folder + product + '/AppDelegate.h',appdh,'utf8');
-		if (result) failExit('Result returned while overwriting product/AppDelegate.h.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product/AppDelegate.h.');
 
 		var appdh = fs.readFileSync(working_folder + product + '/AppDelegate.m','utf8');
 		appdh = appdh.replace(new RegExp(old_product,'g'),product);
@@ -111,24 +108,24 @@ function makeTemplate(options) {
 		appdh = appdh.replace(new RegExp(old_appid,'g'),appid);
 		appdh = appdh.replace(new RegExp(old_key,'g'),key);
 		result = fs.writeFileSync(working_folder + product + '/AppDelegate.m',appdh,'utf8');
-		if (result) failExit('Result returned while overwriting product/AppDelegate.m.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product/AppDelegate.m.');
 
 		xcpj = fs.readFileSync(working_folder + product + '/' + product + '-Prefix.pch','utf8');
 		xcpj = xcpj.replace(new RegExp(old_product,'g'),product);
 		result = fs.writeFileSync(working_folder + product + '/' + product + '-Prefix.pch',xcpj,'utf8');
-		if (result) failExit('Result returned while overwriting product/product-Prefix.pch.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product/product-Prefix.pch.');
 
 		appdh = fs.readFileSync(working_folder + product + '/' + product + '-Info.plist','utf8');
 		appdh = appdh.replace(new RegExp(old_bundle,'g'),bundle);
 		result = fs.writeFileSync(working_folder + product + '/' + product + '-Info.plist',appdh,'utf8');
-		if (result) failExit('Result returned while overwriting product/AppDelegate.m.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product/AppDelegate.m.');
 
 		appdh = fs.readFileSync(working_folder + product + '/ViewController.h','utf8');
 		appdh = appdh.replace(new RegExp(old_product,'g'),product);
 		appdh = appdh.replace(new RegExp(old_org,'g'),org);
 		appdh = appdh.replace(new RegExp(old_name,'g'),name);
 		result = fs.writeFileSync(working_folder + product + '/ViewController.h',appdh,'utf8');
-		if (result) failExit('Result returned while overwriting product/ViewController.h.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product/ViewController.h.');
 
 		appdh = fs.readFileSync(working_folder + product + '/ViewController.m','utf8');
 		appdh = appdh.replace(new RegExp(old_product,'g'),product);
@@ -137,36 +134,36 @@ function makeTemplate(options) {
 		appdh = appdh.replace(new RegExp(old_appid,'g'),appid);
 		appdh = appdh.replace(new RegExp(old_key,'g'),key);
 		result = fs.writeFileSync(working_folder + product + '/ViewController.m',appdh,'utf8');
-		if (result) failExit('Result returned while overwriting product/ViewController.m.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product/ViewController.m.');
 
 		appdh = fs.readFileSync(working_folder + product + '/main.m','utf8');
 		appdh = appdh.replace(new RegExp(old_product,'g'),product);
 		appdh = appdh.replace(new RegExp(old_org,'g'),org);
 		appdh = appdh.replace(new RegExp(old_name,'g'),name);
 		result = fs.writeFileSync(working_folder + product + '/main.m',appdh,'utf8');
-		if (result) failExit('Result returned while overwriting product/main.m.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product/main.m.');
 
 		xcpj = fs.readFileSync(working_folder + product + '.xcodeproj/project.xcworkspace/contents.xcworkspacedata','utf8');
 		xcpj = xcpj.replace(new RegExp(old_product,'g'),product);
 		result = fs.writeFileSync(working_folder + product + '.xcodeproj/project.xcworkspace/contents.xcworkspacedata',xcpj,'utf8');
-		if (result) failExit('Result returned while overwriting product.xcodeproj/project.xcworkspace/contents.xcworkspacedata.');
+		if (result) exitWithMessageAndCode('Result returned while overwriting product.xcodeproj/project.xcworkspace/contents.xcworkspacedata.');
 
 		var zipCommand = 'cd ' + working_folder + ' && zip -q -r ../.' + output_folder + '/' + product + '.zip * && cd ../../';		
 		result = execSync.exec(zipCommand);
-		if (result.code) failExit('Return code ' + result.code + ' while zipping project for token: ' + ourToken);
+		if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while zipping project for token: ' + ourToken);
 
-
-		failExit('Just kidding.. all good.',0);
+		exitWithMessageAndCode(output_folder + '/' + product + '.zip',0);
 
 	} else {
-		failExit('Invalid template',10);
+		exitWithMessageAndCode('Invalid template',10);
 	}
 
 }
+exports.makeTemplate = makeTemplate;
 
-function failExit(reason, code) { 
+function exitWithMessageAndCode(message, code) { 
 
-	console.log('Fatal error: ' + reason);
-	process.exit(code >= 0 ? code : -1);
+	console.log((code == 0 ? message : 'Fatal error: ' + message));
+	process.exit(code >= 0 ? code * -1 : -1);
 
 }
