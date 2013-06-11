8fc50d07964fc2937e1e666753923415a8190ab4
Fosco Marotto <gfosco@gmail.com>
2013-06-11 15:41:05 -0700

    Starting to redo things.

---
+++ cleanUtility.js
@@ -0,0 +1,20 @@
+// Really Quick Start - Cleaner Utility.
+
+var fs = require('fs');
+var execSync = require('execSync');
+
+var cleanCommand = 'rm -rf ./working_folder/*';
+result = execSync.exec(cleanCommand);
+if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while cleaning up working folders');
+
+cleanCommand = 'rm -rf ./output_folder/*';
+result = execSync.exec(cleanCommand);
+if (result.code) exitWithMessageAndCode('Return code ' + result.code + ' while cleaning up output folders');
+
+
+function exitWithMessageAndCode(message, code) { 
+
+	console.log((code == 0 ? message : 'Fatal error: ' + message));
+	process.exit(code >= 0 ? code * -1 : -1);
+
+}
